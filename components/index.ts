44af905f3cbc333172118f8860a1fbb00e619ec1
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-31 01:52:37 +1000

    Add CardCard, Car details, Customfilter and Showmore section. Finished project but API will not work as the site did not let me subscribe currently

--- components/index.ts
+++ components/index.ts
@@ -1,12 +1,18 @@
 "use client"
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer"
 
 export {
     Hero,
     CustomButton,
     NavBar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
 };
